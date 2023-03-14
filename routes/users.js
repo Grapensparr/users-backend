7453129348c262d183603801d748cbab2be078c5
Grapensparr <liza.grapensparr@gmail.com>
2023-03-14 09:26:26 +0100

    Users added

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: 1, name: "Alice"},
+  {id: 2, name: "Bob"},
+  {id: 3, name: "Jane"},
+  {id: 4, name: "John"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.json(users);
 });
 
 module.exports = router;
